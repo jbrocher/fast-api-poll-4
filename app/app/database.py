3edffbc0814b0dfab7c97af4ebf43528be2d924a
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-08 22:14:45 +0100

    build(database): Set up sqlalchemy

---
+++ app/app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from . import config
+
+settings = config.Settings()
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}/{settings.POSTGRES_DATABASE}"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# This will  allow us to create session with this config everytime
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
