01233c70021ac0dbfc5a4b0304fb9ff18649baed
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-11 11:15:44 +0100

    feature(poll): Create question and choice models

--- app/app/alembic/env.py
+++ app/app/alembic/env.py
@@ -4,7 +4,10 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
+import polls.models
 from app.config import Settings
+from app.database import Base
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +21,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 settings = Settings()
 
 # other values from the config, defined by the needs of env.py,
@@ -46,6 +49,7 @@ def run_migrations_offline():
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_server_default=True,
     )
 
     with context.begin_transaction():
@@ -67,7 +71,11 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_server_default=True,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
---
+++ app/polls/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+
+class Question(Base):
+    __tablename__ = "poll_question"
+
+    id = Column(Integer, unique=True, primary_key=True)
+    question_text = Column(String(200), nullable=False)
+    pub_date = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+    choices = relationship("Choice", backref="question")
+
+
+class Choice(Base):
+    __tablename__ = "poll_choice"
+
+    id = Column(Integer, unique=True, primary_key=True)
+    choice_text = Column(String(200), nullable=False)
+    votes = Column(Integer, default=0, nullable=False)
+    question = Column(Integer, ForeignKey("poll_question.id"))
