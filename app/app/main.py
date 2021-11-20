75323baffa2da24fa4fd9778d5d3b4e468e32687
Jean-Baptiste Rocher <jeanb.rocher@gmail.com>
2021-11-20 15:07:09 +0100

    feature(api): Set cors headers

--- app/app/main.py
+++ app/app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import polls.endpoints
+
+origins = ["http://localhost:3000"]
 
 app = FastAPI()
 
 app.include_router(polls.endpoints.router, prefix="/polls")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
